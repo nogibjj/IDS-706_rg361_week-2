6ce9815d7d6e7faa78ecc0b7eb1719d370a64b1c
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-09-08 17:36:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 """ Function to return the descriptive statistics of a Pandas Dataframe"""
 
-def descriptive_stats(df):
-  return df.describe()
+def descriptive_stats(df, col=None):
+    if col == None:
+        col = len(df.axes[1])-1
+        pass
+    return df.iloc[:, col].mean()
