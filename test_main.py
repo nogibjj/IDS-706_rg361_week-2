2741ffc8478c77f7f1738abbfd3eea8dfc118838
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-09-08 17:18:51 -0400

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,15 @@
+"""Test file to verify main function"""
+
+import pandas as pd
+from main import descriptive_stat
+
+def test_stat()
+    # initialize list elements
+    data = [10,20,30,40,50,60]
+      
+    # Create the pandas DataFrame with column name is provided explicitly
+    df = pd.DataFrame(data, columns=['Numbers'])
+    
+    assert df.describe == descriptive_stat(df)
+
+test_stat()
