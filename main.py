f65255546cc6ef209409bd4517abc3a5996409dc
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-09-11 20:26:00 +0000

    added graoh function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """ Function to return the descriptive statistics of a Pandas Dataframe"""
 import pandas as pd
 
+# %%
+import matplotlib.pyplot as plt
+
 
 def descriptive_stats(fname, col=None):
     df = pd.read_csv(fname)
@@ -9,5 +12,15 @@ def descriptive_stats(fname, col=None):
         col = len(df.axes[1]) - 1
     else:
         col = col - 1
+
+    col_name = df.columns[col]
+
+    plt.hist(df[col_name])  # .plot()
+    plt.ylabel("Count of " + col_name)
+    plt.xlabel(col_name)
+    plt.title("Data Laoded from : " + fname)
+    plt.show()
+    plt.savefig("./resources/output.png")
+    plt.clf()
 
     return [df.iloc[:, col].mean(), df.iloc[:, col].median(), df.iloc[:, col].std()]
